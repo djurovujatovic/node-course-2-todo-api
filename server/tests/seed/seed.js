976be6b1e41dce12bdc2e3ed1ab452b9ff2569dd
Djuro Vujatovic <djuro.vujatovic@milestonetech.com>
2017-08-21 14:38:54 -0600

    .

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,48 @@
+const {ObjectID} = require('mongodb');
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+const jwt = require('jsonwebtoken');
+
+
+const userOneID = new ObjectID();
+const userTwoID = new ObjectID();
+
+const users = [{
+	_id: new ObjectID(),
+	email: 'test@testlocker.net',
+	password: 'userPassOne',
+	tokens: [{
+		access: 'auth',
+		token: jwt.sign({_id: userOneID, access: 'auth'}, 'abc123').toString()
+	}]
+}, {
+	_id: userTwoID,
+	email: 'test2@testlocker.net',
+	password: 'userPassTwo'
+}];
+
+const todos = [{
+	_id: new ObjectID(),
+	text: 'First test'
+}, {
+	_id: new ObjectID(),
+	text: 'Second test',
+	completed: true
+}];
+
+const populateTodos = (done) => {
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());
+};
+
+const populateUsers = (done) => {
+	User.remove({}).then(() => {
+		var userOne = new User(users[0]).save();
+		var userTwo = new User(users[1]).save();
+
+		return Promise.all([userOne, userTwo]);
+	}).then(() => done());
+};
+
+module.exports = {todos, populateTodos, users, populateUsers};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,20 +4,10 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 const {ObjectID} = require('mongodb');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos = [{
-	_id: new ObjectID(),
-	text: 'First test'
-}, {
-	_id: new ObjectID(),
-	text: 'Second test'
-}];
-
-beforeEach((done) => {
-	Todo.remove({}).then(() => {
-		return Todo.insertMany(todos);
-	}).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
 	it('should create a new todo', (done) => {
