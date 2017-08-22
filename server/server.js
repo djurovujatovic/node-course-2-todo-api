c0537462d8d0d81d1e43d925c9a70d1fd3a555cd
Djuro Vujatovic <djuro.vujatovic@milestonetech.com>
2017-08-22 14:51:09 -0600

    .

--- server/server.js
+++ server/server.js
@@ -122,6 +122,17 @@ app.get('/users/me', authenticate, (req, res) => {
 });
 
 
+app.post('/users/login', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+
+	User.findByCredentials(body.email, body.password).then((user) => {
+		User.generateAuthToken().then((token) => {
+			res.header('x-auth', token).send(user);
+		});
+	}).catch(e) => {
+		res.status(400).send();
+	};
+});
 
 
 app.listen(port, () => {
